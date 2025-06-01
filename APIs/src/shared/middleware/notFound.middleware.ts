dd521eb9c1ff8919b19afd4cdf0403f4c81c295a
AhmedMashali <ahmedmashali1999@gmail.com>
2025-06-01 20:42:30 +0300

    Add dto validation middleware

--- APIs/src/shared/middleware/notFound.middleware.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-import { errorResponse } from '../utils/apiResponse';
-
-export const notFoundHandler = (req: Request, res: Response, next: NextFunction) => {
-    errorResponse(res, 'Endpoint not found', 404);
-};
