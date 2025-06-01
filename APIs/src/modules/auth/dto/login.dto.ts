eb65ae2e5d112a8739ba428fdcef6435d413f4fc
AhmedMashali <ahmedmashali1999@gmail.com>
2025-06-01 20:45:26 +0300

    Add auth dtos

---
+++ APIs/src/modules/auth/dto/login.dto.ts
@@ -0,0 +1,13 @@
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+
+export class LoginDto {
+    @IsNotEmpty()
+    @IsString()
+    username!: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(8, { message: 'Password must be at least 8 characters long' })
+    @MaxLength(32, { message: 'Password must be fewer than 32 characters long' })
+    password!: string;
+}
